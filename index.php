97b7831818808034446f21238a7ddbe6232f1c5c
AdrianaSantorio <adriana.santorio@gmail.com>
2022-02-28 16:21:49 +0100

    printed faqs

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$faq = [
+$faqs = [
     [
         'question' => 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
         'answer' => [
@@ -84,11 +84,23 @@ $faq = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Google FAQ</title>
 </head>
 
 <body>
-
+    <main>
+        <?php foreach ($faqs as $faq) { ?>
+            <h2>
+                <?= $faq['question'] ?>
+            </h2>
+            <?php foreach ($faq['answer'] as $paragraph) { ?>
+                <p>
+                    <?= $paragraph ?>
+                </p>
+            <?php } ?>
+        <?php } ?>
+    </main>
 </body>
 
 </html>
